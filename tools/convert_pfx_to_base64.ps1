123f31b0bcf5f1b86b7a84e83867a795e570649d
S3COPS <kev@scranbag.com>
2019-08-06 12:14:13 +0100

    Update convert_pfx_to_base64.ps1

--- tools/convert_pfx_to_base64.ps1
+++ tools/convert_pfx_to_base64.ps1
@@ -5,10 +5,17 @@ function Get-FileName($initialDirectory)
 
     $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
     $OpenFileDialog.initialDirectory = $initialDirectory
-    $OpenFileDialog.filter = "All files (*.*)| *.*"
+    $OpenFileDialog.filter = "All files (*.pfx)| *.pfx"
     $OpenFileDialog.ShowDialog() | Out-Null
     $OpenFileDialog.filename
 }
+Write-Host "Browse to the pfx certificate file to be encoded"
+Start-Sleep -s 1
 $file = get-filename
+$filePath = Split-Path -Path $file -Parent
+
 $fileContentBytes = get-content $file -Encoding Byte
-[System.Convert]::ToBase64String($fileContentBytes) | Out-File ‘pfx-encoded-bytes.txt’
+[System.Convert]::ToBase64String($fileContentBytes) | Out-File  $filePath\'pfx-encoded.txt'
+Start-Sleep -s 1
+Write-Host "The Base64 encoded Certificate has been written to"
+Write-Host $filePath
